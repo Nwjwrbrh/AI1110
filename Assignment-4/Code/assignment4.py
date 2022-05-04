b465d8bb8e7db37708c864285f51823bbe6fd3e6
Nwjwr Khungur Brahma <100432233+Nwjwrbrh@users.noreply.github.com>
2022-05-04 20:19:27 +0530

    Add files via upload

--- Assignment-4/Code/assignment4.py
+++ Assignment-4/Code/assignment4.py
@@ -1,1 +1,10 @@
-
+import math
+def div(a,b):
+    return a/b
+print('Lets assume there are 24 balls in a bag with 2 two colours blue and green')
+print('Please enter the probabilty of randomly drawing a green ball')
+num=int(input('numerator :'))
+den=int(input('denominator :'))
+x= div(num,den)
+y=(1-x)*24
+print('then the number of blue balls present is',y)
