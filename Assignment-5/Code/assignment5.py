c0a0d3be1c29a9047dab06fac8fc4dbeef9bc83b
Nwjwr Khungur Brahma <nikenwjwr10@gmail.com>
2022-05-10 11:34:05 +0530

    made some changes

--- Assignment-5/Code/assignment5.py
+++ Assignment-5/Code/assignment5.py
@@ -19,28 +19,30 @@ cnt_4=0
 for i in range(1,7):
     for j in range(1,7):
         if i == 5 and i + j > 9:
-            cnt_1=cnt_1+1 
+            cnt_1+=1 
         if i + j == 8 and j < 4:
-            cnt_2=cnt_2+1
+            cnt_2+=1
         if i == 5:
-            cnt_3=cnt_3+1
+            cnt_3+=1
         if j < 4:
-            cnt_4=cnt_4+1
+            cnt_4+=1
 tp_1=div(cnt_1,cnt_3)
 tp_2=div(cnt_2,cnt_4)
 
 #for experimental probability using random variable
-#I just took the no. of observation = no. of items in sample space
+#I just took the no. of observation be 10000
+obs=10000
 crt_1=0
 crt_3=0
-x=rn.randint(1,7,size = N)
-y=rn.randint(1,7,size = N)
+x=rn.randint(1,7,size = obs)
+y=rn.randint(1,7,size = obs)
 z=np.column_stack((x,y))
-for i in range(0,36):
+print(z)
+for i in range(0,obs):
     if z[i][0]==5 and  z[i][0]+z[i][1]>9:
-        crt_1=crt_1+1
+        crt_1+=1
     if z[i][1]<4 and z[i][0]+z[i][1]==8:
-        crt_3=crt_3+1
+        crt_3+=1
 crt_2=np.count_nonzero(x==5)
 crt_4=np.count_nonzero(y<4)
 ep_1=div(crt_1,crt_2)
